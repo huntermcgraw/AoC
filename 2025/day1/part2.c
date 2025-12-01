878086b75bb35b8c9eb1bc612631f1997824285a
Hunter <huntercmcgraw@gmail.com>
2025-12-01 12:08:00 -0500

    pt2 in progress

---
+++ 2025/day1/part2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    FILE *file = fopen("input.txt", "r");
+    
+    int position = 50;
+    int count = 0;
+    int value, prev_pos;
+    int rotate_right;
+    char line[6];
+
+    printf("Current position: %d\n", position);
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        prev_pos = position;
+        rotate_right = line[0] == 'R' ? 1 : 0;
+        value = atoi(line+1);
+        count += (value / 100);
+        if (rotate_right) {
+            position += value;
+        } else {
+            position -= value;
+        }
+        position %= 100;
+        if (position < 0) {
+            if (position + value != 0)
+                count++;
+            position += 100;
+        } else if (position < prev_pos && rotate_right && position != 0) {
+            count++;
+        } else if (value < 100 && position == 0) {
+            count++;
+        }
+        printf("The dial is rotated %s to point at %d.\n", line, position);
+        printf("Current count: %d\n\n", count);
+    }
+
+    fclose(file);
+    
+    printf("Password: %d\n", count);
+
+    return 0;
+}
