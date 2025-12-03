83dd60da9580bc926ed8d0248b15370017e02d33
Hunter <huntercmcgraw@gmail.com>
2025-12-03 16:33:54 -0500

    update part 1

--- 2025/day3/part1.c
+++ 2025/day3/part1.c
@@ -2,40 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-unsigned long long calc_line(char *line);
+unsigned long long calc_line(char *line, int line_length);
 
 int main() {
     
-    FILE *file = fopen("input.txt", "r");
+    FILE *file = fopen("example.txt", "r");
 
-    char line[102]; //17
+    char line[102];
     unsigned long long sum = 0;
 
     fgets(line, sizeof(line), file);
-    sum += calc_line(line);
-
     int line_length = strlen(line);
-    printf("Line length: %d\n", line_length);
+
+    sum += calc_line(line, line_length);
 
     while (fgets(line, sizeof(line), file) != NULL) {
-        sum += calc_line(line);
+        sum += calc_line(line, line_length);
     }
 
     fclose(file);
     
-    printf("Total output joltage: %d\n", sum);
+    printf("Total output joltage: %llu\n", sum);
 
     return 0;
 }
 
-unsigned long long calc_line(char *line) {
+unsigned long long calc_line(char *line, int line_length) {
     
     int highest_first_digit = line[0] - '0';
-    int highest_digits[102]; //17
+    int highest_digits[line_length];
     int highest_digit_count = 0;
     highest_digits[highest_digit_count] = 0;
     highest_digit_count++;
-    for(int i = 1; i < 99; i++) { //14
+    for(int i = 1; i < line_length - 2; i++) { 
         if (line[i] - '0' > highest_first_digit) {
             highest_first_digit = line[i] - '0';
             highest_digit_count = 0;
@@ -48,7 +47,7 @@ unsigned long long calc_line(char *line) {
     }
     int highest_second_digit = -1;
     for (int i = 0; i < highest_digit_count; i++) {
-        for (int j = highest_digits[i] + 1; j < 100; j++) { //15
+        for (int j = highest_digits[i] + 1; j < line_length - 1; j++) {
             if (line[j] == '9') {
                 highest_second_digit = line[j] - '0';
                 break;
