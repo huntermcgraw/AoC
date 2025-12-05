29eb84add464d5f9877eb9b0903f7e7d4f4e85d2
Hunter <huntercmcgraw@gmail.com>
2025-12-05 02:07:45 -0500

    working on pt 2

---
+++ 2025/day5/part2.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    
+    FILE *file = fopen("input.txt", "r"); // 369318224397456 too high / not 366558057869922 / not 346641541043583
+    
+    int num_lines = 200;
+    int length = 50;
+
+    unsigned long long sum = 0;
+    char ranges[num_lines][length];
+    unsigned long long range_starts[num_lines];
+    unsigned long long range_ends[num_lines];
+    char line[length];
+    char *start;
+    char *end;
+    unsigned long long id_value;
+
+    int i = 0;
+    while (fgets(ranges[i], sizeof(ranges[i]), file) != NULL) {
+        if (ranges[i][0] == '\n') {
+            break;
+        }
+        printf("Range: %s", ranges[i]);
+        start = strtok(ranges[i], "-");
+        end = strtok(NULL, "-");
+        range_starts[i] = strtoull(start, NULL, 10);
+        range_ends[i] = strtoull(end, NULL, 10);
+        i++;
+    }
+
+    // sort ranges by lowest start value
+
+    for (int k = 0; k < i; k++) {
+        // use two pointers to mark the beginning and end of a simulated merge
+
+        //check if range_ends[k] overlaps with any range_starts[j] where j > k
+        sum += range_ends[k] - range_starts[k] + 1;
+
+        //k += current_end pointer
+    }
+
+    fclose(file);
+    
+    printf("Fresh ingredient IDs: %llu\n", sum);
+
+    return 0;
+}
